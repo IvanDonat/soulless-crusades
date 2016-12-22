e0758a19532dfb33dbc18c6184eaa310f59bf137
Roko Milić <cybermen21@hotmail.com>
2016-12-23 00:44:18 +0100

    Added SetPlayerName

---
+++ Assets/Scripts/Misc/SetPlayerName.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(InputField))]
+public class SetPlayerName : MonoBehaviour
+{
+    public InputField inputField;
+    static string playerNamePrefKey = "PlayerName";
+
+    void Start()
+    {
+        string defaultName = ""; //ovdje mozemo napravit ako oces tipa guest + random(0,9999)
+        inputField = GetComponent<InputField>();
+
+        if (inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                inputField.text = defaultName;
+            }
+        }
+
+        PhotonNetwork.playerName = defaultName;
+    }
+
+    public void SetName(string value)
+    {
+        PhotonNetwork.playerName = value;
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
