cdb1acc1f2088a0924cb57f5c7b22422a3d8dbc4
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-12-22 22:10:06 +0100

    Elementary player movement

---
+++ Assets/Scripts/Misc/DestroyAfterTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterTime : MonoBehaviour {
+    public float lifetimeInSeconds = 2f;
+
+    void Update()
+    {
+        lifetimeInSeconds -= Time.deltaTime;
+
+        if (lifetimeInSeconds <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private float defaultFriction = 3f; // friction drops when hit by spell
+
+    private Rigidbody rbody;
+    private Vector3 targetPosition;
+
+    private Transform terrain;
+
+    public Transform prefabParticlesOnClick;
+
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody>();
+        rbody.freezeRotation = true;
+        rbody.drag = defaultFriction;
+
+        terrain = GameObject.FindGameObjectWithTag("Terrain").transform;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            // @TODO spawn particles at click pos
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (terrain.GetComponent<Collider>().Raycast(ray, out hit, Mathf.Infinity))
+            {
+                SetTargetPosition(hit.point);
+
+                GameObject.Instantiate(prefabParticlesOnClick, hit.point + Vector3.up * 0.1f, Quaternion.identity);
+            }
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rbody.drag = Mathf.Lerp(rbody.drag, defaultFriction, Time.time);
+
+        targetPosition.y = transform.position.y; // top-down doesn't matter
+        float distToTarget = (targetPosition - transform.position).magnitude;
+
+        if (distToTarget > 0.5f)
+        { // apply force towards target
+            Vector3 force = targetPosition - transform.position;
+            force.Normalize();
+
+            rbody.AddForce(force * 30, ForceMode.Acceleration);
+        }
+    }
+
+    public void SetTargetPosition(Vector3 pos)
+    {
+        targetPosition = pos;
+    }
+}
