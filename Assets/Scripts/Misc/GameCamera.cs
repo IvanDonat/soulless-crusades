a8726d12ee7466a5779285f98ad82b591c1d70ae
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-12-23 12:52:11 +0100

    Game Camera Script

---
+++ Assets/Scripts/Misc/GameCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour {
+    private Transform myPlayer;
+    Vector3 offset = new Vector3(0, 15, -25);
+
+    private float speed = 10f;
+    private float mouseDeadzone = 0.05f;
+
+    private float minX = -10f, maxX = 10f, minZ = -10f, maxZ = 10f;
+
+    void Start()
+    {
+        transform.position = offset;
+    }
+
+    void LateUpdate()
+    {
+        // mousePos is a vector from 0 to 1 for each axis
+        Vector2 mousePos = Input.mousePosition;
+        mousePos.x /= Screen.width;
+        mousePos.y /= Screen.height;
+
+        Vector3 newPos = transform.position;
+
+        if (mousePos.x < mouseDeadzone)
+        { // move left
+            newPos += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (mousePos.x > 1f - mouseDeadzone)
+        { // move right
+            newPos += Vector3.right * speed * Time.deltaTime;
+        }
+
+        if (mousePos.y < mouseDeadzone)
+        { // move back
+            newPos += Vector3.back * speed * Time.deltaTime;
+        }
+
+        if (mousePos.y > 1f - mouseDeadzone)
+        { // move forward
+            newPos += Vector3.forward * speed * Time.deltaTime;
+        }
+      
+        newPos.x = Mathf.Clamp(newPos.x, minX + offset.x, maxX + offset.x);
+        newPos.y = transform.position.y;
+        newPos.z = Mathf.Clamp(newPos.z, minZ + offset.z, maxZ + offset.z);
+
+        transform.position = newPos;
+    }
+}
