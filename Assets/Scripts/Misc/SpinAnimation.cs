368688642da48fa2c560098699334c0cf9ce62ee
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-12-23 15:24:38 +0100

    New UI

--- Assets/Scripts/Misc/SpinAnimation.cs
+++ Assets/Scripts/Misc/SpinAnimation.cs
@@ -3,47 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpinAnimation : MonoBehaviour {
-
     public float speed = 360f;
-    public float radius = 0.5f;
-
-    public GameObject particles;
-
-    Vector3 offset;
-    Transform transform;
-    Transform particleTransform;
-
-    bool isAnimating;
-
-    void Awake()
-    {
-        particleTransform = particles.GetComponent<Transform>();
-        transform = GetComponent<Transform>();
-    }
-
-    void Start()
-    {
-        StartLoaderAnimation();
-    }
 
     void Update()
     {
-        if (isAnimating)
-        {
-            transform.Rotate(0f, 0f, speed * Time.deltaTime);
-            particleTransform.localPosition = Vector3.MoveTowards(particleTransform.localPosition, offset, 0.5f * Time.deltaTime);
-        }
-    }
-
-    public void StartLoaderAnimation()
-    {
-        isAnimating = true;
-        offset = new Vector3(radius, 0f, 0f);
-        particles.SetActive(true);
-    }
-
-    public void StopLoaderAnimation()
-    {
-        particles.SetActive(false);
+        transform.Rotate(0, 0, -speed * Time.deltaTime);
     }
 }
