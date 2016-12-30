28accb8a736f4d697320576626bd9ff2a05c494e
Ivan Donat Pupovac <ivandonat@gmail.com>
2016-12-30 14:37:04 +0100

    UI sound system

---
+++ Assets/Scripts/Misc/UISoundScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISoundScript : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler {
+    // add this script to each UI object that should make sound fx
+
+    //private AudioSource hoverSound;
+    private AudioSource clickSound;
+
+    void Start()
+    {
+        //hoverSound = GameObject.Find("Hover Sound").GetComponent<AudioSource>();
+        clickSound = GameObject.Find("Click Sound").GetComponent<AudioSource>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //hoverSound.Play();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        clickSound.Play();
+    }
+}
